eb949efab63cfcef1a1180bb8f1a445cdf1fb5be
Guillermo Izalde <izalde.guillermo45@gmail.com>
2021-12-13 05:18:57 -0600

    Ultimas Configuraciones

--- app/src/main/java/com/example/stores/StoreDao.kt
+++ app/src/main/java/com/example/stores/StoreDao.kt
@@ -5,15 +5,19 @@ import androidx.room.*
 
 @Dao
 interface StoreDao {
-    @Query("SELECT * FROM storeEntity")
+    @Query("SELECT * FROM StoreEntity")
     fun getAllStores() : MutableList<StoreEntity>
 
+    @Query("SELECT * FROM StoreEntity where id = :id")
+    fun getStoreById(id: Long): StoreEntity
+
     @Insert
-    fun addStore(storeEntity: StoreEntity)
+    fun addStore(storeEntity: StoreEntity): Long
 
     @Update
     fun updateStore(storeEntity: StoreEntity)
 
     @Delete
     fun deleteStore(storeEntity: StoreEntity)
+
 }
