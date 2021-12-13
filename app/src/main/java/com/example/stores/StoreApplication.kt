991e0df71471e8ce053b6d23a25078a0977c785e
Guillermo Izalde <izalde.guillermo45@gmail.com>
2021-12-13 04:47:03 -0600

    Clse String

--- app/src/main/java/com/example/stores/StoreApplication.kt
+++ app/src/main/java/com/example/stores/StoreApplication.kt
@@ -15,5 +15,26 @@ class StoreApplication : Application() {
             StoreDatabase::class.java,
             "StoreDatabase")
             .build()
-    }
+
+        class StoreApplication : Application() {
+            companion object{
+                lateinit var database: StoreDatabase
+            }
+
+            override fun onCreate() {
+                super.onCreate()
+
+                val MIGRATION_1_2 = object : Migration(1, 2){
+                    override fun migrate(database: SupportSQLiteDatabase) {
+                        database.execSQL("ALTER TABLE StoreEntity ADD COLUMN photoUrl TEXT NOT NULL DEFAULT ''")
+                    }
+                }
+
+                database = Room.databaseBuilder(this,
+                    StoreDatabase::class.java,
+                    "StoreDatabase")
+                    .addMigrations(MIGRATION_1_2)
+                    .build()
+
+            }
 }
